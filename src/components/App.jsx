09f43d873761548761e50c42d791430ffebc1dcf
Kate Panchenko <k.panchenko12345@gmail.com>
2023-03-03 20:20:29 +0200

    step 3 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,24 @@ import { Component } from "react";
 import { ContactForm } from "./ContactForm/ContactForm";
 import { Contacts } from "./Contacts/Contacts";
 import { nanoid } from 'nanoid';
+import { Filter } from "./Contacts/Filter/Filter";
 
 export class App extends Component {
   state = {
-  contacts: [],
+    contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
   name: '',
   number: '',
+  filter: '',
   }
+
+  contactFilter = event => {
+    this.setState({ filter: event.target.value })
+  };
 
   addContact = contact => {
     if (this.state.contacts.find(user => user.name === contact.name)) {
@@ -27,11 +38,17 @@ export class App extends Component {
   }
 
   render() {
+    const lowerCaseFilter = this.state.filter.toLowerCase();
+    const filteredContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(lowerCaseFilter));
     return (
       <Layout>
         <GlobalStyle />
+        <h2>Phonebook</h2>
         <ContactForm onSubmit={this.addContact} />
-        <Contacts contacts={this.state.contacts} />
+        <h2>Contacts</h2>
+        <Filter value={ this.state.filter} onChange={this.contactFilter} />
+        <Contacts contacts={filteredContacts} />
         {/* {this.state.contacts[0]
           ? <Contacts contact={this.state.contacts} />
           : <p>There’s nothing here yet...</p>} */}
---
+++ src/components/Contacts/Filter/Filter.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+export const Filter = ({ value, onChange }) => {
+  return (
+    <form>
+      <label>
+        Find contacts by name
+        <input type="text" value={value} onChange={onChange} />
+      </label>
+    </form>
+  );
+};
+
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
